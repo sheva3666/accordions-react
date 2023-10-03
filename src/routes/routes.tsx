423ec06bd7663bf9129626ee97ba2fe30f9f9016
sheva3666 <ivan.shevchenko@sap.com>
2023-10-03 07:43:19 +0200

    implemented parallax

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,12 @@
 import { AccordionOne } from "../components/AccordionOne/AccordionOne";
+import { Parallax } from "../components/Parallax/Parallax";
 
 export const routeUrls = { accordions: "/accordions", home: "/", parallax: "/paralax"}
 
 export const routes = [
     {path: routeUrls.accordions, element: <AccordionOne/>, name: "Accordion One"},
     {path: routeUrls.home, element: <></>, name: "Home"},
-    {path: routeUrls.parallax, element: <></>, name: "Paralax"}
+    {path: routeUrls.parallax, element: <Parallax/>, name: "Paralax"}
 
 
 ]
